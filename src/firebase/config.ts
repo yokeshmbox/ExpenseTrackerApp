8694dbccb80d624e536306268c87b6f7c6c6e7b6
yokeshmbox <yokeshmbox@gmail.com>
2025-12-25 21:54:51 +0530

    fix config

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,9 @@
+import { env } from "process";
+
 export const firebaseConfig = {
   "projectId": "studio-1968949760-f47e3",
   "appId": "1:1735932162:web:78370c4aeae8f79e39aa1b",
-  "apiKey": "AIzaSyAO0QUOv0q35T4oHW9bp9VyB0zyilJc1cQ",
+  "apiKey": env.FIREBASE_API_KEY,
   "authDomain": "studio-1968949760-f47e3.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "1735932162"
